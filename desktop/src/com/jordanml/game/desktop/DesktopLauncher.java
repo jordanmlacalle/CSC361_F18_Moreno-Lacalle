f22a5e51dda4b9cbccc964bdd6f11f95c2c2d909
Jordan Moreno-Lacalle <jordanmlacalle@gmail.com>
2018-10-23 20:32:49 -0400

    Add base libgdx project files

---
+++ desktop/src/com/jordanml/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jordanml.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jordanml.game.CSC361_F18_Moreno-Lacalle;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CSC361_F18_Moreno-Lacalle(), config);
+	}
+}
