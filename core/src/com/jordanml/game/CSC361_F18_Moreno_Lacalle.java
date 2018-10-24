3be2c91300d51e40d9f36a0dc3cb5062a72e9949
Jordan Moreno-Lacalle <jordanmlacalle@gmail.com>
2018-10-24 02:33:38 -0400

    Initial implementation of primary game class

--- core/src/com/jordanml/game/CSC361_F18_Moreno_Lacalle.java
+++ core/src/com/jordanml/game/CSC361_F18_Moreno_Lacalle.java
@@ -1,33 +1,27 @@
 package com.jordanml.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.assets.AssetManager;
 
-public class CSC361_F18_Moreno_Lacalle extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+import com.jordanml.game.assets.Assets;
+import com.jordanml.game.screens.GameScreen;
 
+
+public class CSC361_F18_Moreno_Lacalle extends Game
+{	
+	/**
+	 * Creates the game and starts it from the GameScreen
+	 */
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+    public void create()
+	{
+      // Set Libgdx log level
+      Gdx.app.setLogLevel(Application.LOG_DEBUG);
+      // Load assets
+      Assets.instance.init(new AssetManager());
+      // Start game at GameScreen
+      setScreen(new GameScreen(this));
 	}
 }
