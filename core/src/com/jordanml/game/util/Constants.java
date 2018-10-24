d2f615b9523d8f39b7a8328aab3adfaf79559a2c
Jordan Moreno-Lacalle <jordanmlacalle@gmail.com>
2018-10-24 00:44:20 -0400

    Add Constants class

---
+++ core/src/com/jordanml/game/util/Constants.java
@@ -0,0 +1,13 @@
+package com.jordanml.game.util;
+
+/**
+ * Keeps track of constants that may be needed across multiple classes.
+ *
+ */
+public class Constants
+{
+    // Visible game world dimensions (in meters)
+    public static final float VIEWPORT_WIDTH = 5.0f;
+    public static final float VIEWPORT_HEIGHT = 5.0f;
+    
+}
