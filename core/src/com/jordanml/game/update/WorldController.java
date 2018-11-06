4144bc215531f5f4ccc4ac7cd4211b448c358e72
Jordan Moreno-Lacalle <jordanmlacalle@gmail.com>
2018-11-06 11:06:54 -0500

    Update WorldController and WorldRenderer, add packed images

--- core/src/com/jordanml/game/update/WorldController.java
+++ core/src/com/jordanml/game/update/WorldController.java
@@ -1,11 +1,24 @@
 package com.jordanml.game.update;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.Application.ApplicationType;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.utils.Disposable;
+import com.jordanml.game.level.Level;
+import com.jordanml.game.util.Constants;
+import com.jordanml.game.util.CameraHelper;
+
 /**
  * This class will handle most general game logic.
  *
  */
-public class WorldController
+public class WorldController extends InputAdapter
 {
+    public static final String TAG = WorldController.class.getName();
+    public Level level;
+    public CameraHelper cameraHelper;
+    
     public WorldController()
     {
         init();
@@ -16,7 +29,15 @@ public class WorldController
      */
     public void init()
     {
-        
+        // Set world controller as input processor
+        Gdx.input.setInputProcessor(this);
+        cameraHelper = new CameraHelper();
+        initLevel();
+    }
+    
+    private void initLevel()
+    {
+        level = new Level(Constants.LEVEL_01);
     }
     
     /**
@@ -26,6 +47,84 @@ public class WorldController
      */
     public void update(float deltaTime)
     {
+        handleDebugInput(deltaTime);
+        level.update(deltaTime);
+    }
+
+    /**
+     * Handles some special inputs for resetting game world and switching camera target
+     * @param keycode the keycode for the pressed key
+     */
+    @Override
+    public boolean keyUp(int keycode)
+    {
+        // Reset game world
+        if (keycode == Keys.R)
+        {
+            init();
+            Gdx.app.debug(TAG, "Game world reset");
+        }
+        // Toggle camera follow
+        else if (keycode == Keys.ENTER)
+        {
+            cameraHelper.setTarget(null);
+            Gdx.app.debug(TAG, "Camera follow enabled: " + cameraHelper.hasTarget());
+        }
         
+        return false;
+    }
+
+    /**
+     * Handle debug input, allows testing during development. Enables control of
+     * primary (non-gui) camera.
+     * 
+     * @param deltaTime time passed since the previous frame
+     */
+    private void handleDebugInput(float deltaTime)
+    {
+        if (Gdx.app.getType() != ApplicationType.Desktop)
+            return;
+
+        
+        // Camera Controls (move)
+        float camMoveSpeed = 5 * deltaTime;
+        float camMoveSpeedAccelerationFactor = 5;
+        if (Gdx.input.isKeyPressed(Keys.SHIFT_LEFT))
+            camMoveSpeed *= camMoveSpeedAccelerationFactor;
+        if (Gdx.input.isKeyPressed(Keys.LEFT))
+            moveCamera(-camMoveSpeed, 0);
+        if (Gdx.input.isKeyPressed(Keys.RIGHT))
+            moveCamera(camMoveSpeed, 0);
+        if (Gdx.input.isKeyPressed(Keys.UP))
+            moveCamera(0, camMoveSpeed);
+        if (Gdx.input.isKeyPressed(Keys.DOWN))
+            moveCamera(0, -camMoveSpeed);
+        if (Gdx.input.isKeyPressed(Keys.BACKSPACE))
+            cameraHelper.setPosition(0, 0);
+
+        // Camera Controls (zoom)
+        float camZoomSpeed = 1 * deltaTime;
+        float camZoomSpeedAccelerationFactor = 5;
+        if (Gdx.input.isKeyPressed(Keys.SHIFT_LEFT))
+            camZoomSpeed *= camZoomSpeedAccelerationFactor;
+        if (Gdx.input.isKeyPressed(Keys.COMMA))
+            cameraHelper.addZoom(camZoomSpeed);
+        if (Gdx.input.isKeyPressed(Keys.PERIOD))
+            cameraHelper.addZoom(-camZoomSpeed);
+        if (Gdx.input.isKeyPressed(Keys.SLASH))
+            cameraHelper.setZoom(1);
+    }
+    
+    /**
+     * Move the camera to the coordinates specified by (x, y)
+     * 
+     * @param x
+     * @param y
+     */
+    private void moveCamera(float x, float y)
+    {
+        x += cameraHelper.getPosition().x;
+        y += cameraHelper.getPosition().y;
+        cameraHelper.setPosition(x, y);
     }
 }
--- desktop/src/com/jordanml/game/desktop/DesktopLauncher.java
+++ desktop/src/com/jordanml/game/desktop/DesktopLauncher.java
@@ -2,10 +2,26 @@ package com.jordanml.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 import com.jordanml.game.CSC361_F18_Moreno_Lacalle;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
+public class DesktopLauncher 
+{
+    private static boolean rebuildAtlas = false;
+    private static boolean drawDebugOutline = false;
+    
+	public static void main (String[] arg) 
+	{
+	    if(rebuildAtlas)
+	    {
+	        Settings settings = new Settings();
+            settings.maxWidth = 1024;
+            settings.maxHeight = 1024;
+            settings.debug = drawDebugOutline;
+            TexturePacker.process(settings, "assets-raw/images","images","hallowed");
+	    }
+	    
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new CSC361_F18_Moreno_Lacalle(), config);
 	}
